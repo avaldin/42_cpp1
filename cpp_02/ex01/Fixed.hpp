8ee62a587e71d367d7a57a4d5b689126b6ca0fa6
avaldin <valdinalan.pro@gmail.com>
2024-06-10 15:37:12 +0200

    cpp02 ex01 not finished

---
+++ cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+
+class Fixed
+{
+public:
+
+	// constructor / destructor
+	Fixed();
+	Fixed(int intValue);
+	Fixed(Fixed &fixed);
+	~Fixed();
+
+	// operator
+	Fixed	&operator=(const Fixed &fixed);
+
+	// methods
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+private:
+
+	int					value;
+	static const int	fractionalBits = 8;
+};
+
+
+#endif
